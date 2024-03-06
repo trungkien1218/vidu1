1676efa86f7e85245e34a94f4bff2c648402b77c
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2024-03-06 15:53:31 +0700

    ok

---
+++ vidu1/vidu1--/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './input.css/'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="text-red-600">
+        hdhdhdhhd
+        </div> 
+        <p class="uppercase ...">The quick brown fox ...</p>
+
+        <p class="capitalize ...">The quick brown fox ...</p>
+    </>
+  )
+}
+
+export default App
